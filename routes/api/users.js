318813d56d7531603c245ebe4ef77ffa150bed71
Deepak-Kharah <kharahdeepak009@gmail.com>
2020-03-28 18:38:45 +0530

    user authentication using express-validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/users
+// @route   POST api/users
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => {
-    res.send('user route');
-});
+router.post(
+    '/',
+    [
+        check('username', 'Username is required').not().isEmpty(),
+        check('email', 'Please include a valid Email address').isEmail(),
+        check('password', 'Password must have 6 characters or more').isLength({ min: 6 })
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error: errors.array() });
+        }
+        res.json(req.body);
+    }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect DB
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 // Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
