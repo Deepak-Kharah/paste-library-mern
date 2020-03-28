2a6782bdf5867c006dba086e07905613ab177438
Deepak-Kharah <kharahdeepak009@gmail.com>
2020-03-28 12:11:53 +0530

    initial commit
    setup git and installed node packages

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('API RUNNING');
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
