973c24005cdd1330bf4cb7c2a534ab6d8d8a7cb6
Deepak-Kharah <kharahdeepak009@gmail.com>
2020-04-03 17:16:18 +0530

    refactor auth.js middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,11 +10,16 @@ module.exports = function(req, res, next) {
 
     // Verify the token
     try {
-        const decoded = jwt.verify(token, process.env.JWT_PASSWORD);
-
-        req.user = decoded.user;
-        next();
+        jwt.verify(token, process.env.JWT_PASSWORD, (error, decoded) => {
+            if (error) {
+                res.status(401).json({ msg: 'Token is not valid' });
+            } else {
+                req.user = decoded.user;
+                next();
+            }
+        });
     } catch (err) {
-        res.status(401).json({ msg: 'Token is not valid' });
+        console.error('something wrong with auth middleware');
+        res.status(500).json({ msg: 'Server Error' });
     }
 };
