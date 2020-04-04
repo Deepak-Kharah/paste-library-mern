5bd959c5728c55cb691ee3f10adb3bb13c35074f
Deepak-Kharah <kharahdeepak009@gmail.com>
2020-04-04 13:08:01 +0530

    delete profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -44,4 +44,21 @@ router.post('/', auth, async (req, res) => {
     }
 });
 
+// @route    DELETE api/profile
+// @desc     Delete user profile, user and dumps (maybe)
+// @access   Private
+
+router.delete('/', auth, async (req, res) => {
+    try {
+        // Delete profile
+        await Profile.findOneAndDelete({ user: req.user.id });
+
+        // Delete user
+        await User.findOneAndDelete({ _id: req.user.id });
+        res.json({ msg: 'User deleted' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router;
