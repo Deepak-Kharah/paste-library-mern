908eb4ea12295b3d22ab8537225277f913ff5190
Deepak-Kharah <kharahdeepak009@gmail.com>
2020-04-07 19:00:52 +0530

    initial configuration

---
+++ client/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(jsx|js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.js', '.jsx' ]
+    }
+};
