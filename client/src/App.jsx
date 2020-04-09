0480f76c3dc5393a2483fc5ebd67e205e765fd77
Deepak-Kharah <kharahdeepak009@gmail.com>
2020-04-09 11:55:16 +0530

    setup redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Home from './components/layout/Home';
 import Navbar from './components/layout/Navbar';
@@ -8,19 +9,23 @@ import Register from './components/auth/Register';
 import About from './components/promotion/About';
 import ContactUs from './components/promotion/ContactUs';
 
+import store from './store';
+
 const App = () => (
-    <Router>
-        <Navbar />
-        <div className="container">
-            <Route exact path="/" component={Home} />
-            <Switch>
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/contact-us" component={ContactUs} />
-                <Route exact path="/about" component={About} />
-            </Switch>
-        </div>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <Navbar />
+            <div className="container">
+                <Route exact path="/" component={Home} />
+                <Switch>
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/contact-us" component={ContactUs} />
+                    <Route exact path="/about" component={About} />
+                </Switch>
+            </div>
+        </Router>
+    </Provider>
 );
 
 export default App;
