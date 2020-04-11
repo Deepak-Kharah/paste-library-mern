36a3b2f69fc9d088041d6ab7dbbb91978065ca94
Deepak-Kharah <kharahdeepak009@gmail.com>
2020-04-11 17:09:46 +0530

    created loadUser
    this commit ensures that token is always check and load on refresh. If
    the value is in the localStorage, then it will automatically add to the
    header so that other request dont have to explicitly add it.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import About from './components/promotion/About';
 import Alert from './components/layout/Alert';
@@ -9,25 +10,43 @@ import Home from './components/layout/Home';
 import Login from './components/auth/Login';
 import Navbar from './components/layout/Navbar';
 import Register from './components/auth/Register';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
 
 import store from './store';
 
-const App = () => (
-    <Provider store={store}>
-        <Router>
-            <Navbar />
-            <div className="container">
-                <Alert />
-                <Route exact path="/" component={Home} />
-                <Switch>
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/register" component={Register} />
-                    <Route exact path="/contact-us" component={ContactUs} />
-                    <Route exact path="/about" component={About} />
-                </Switch>
-            </div>
-        </Router>
-    </Provider>
-);
+if (localStorage.token) {
+    setAuthToken(localStorage.token);
+}
+
+class App extends Component {
+    componentDidMount() {
+        store.dispatch(loadUser());
+        console.log('i am used');
+    }
+    static propTypes = {
+        loadUser: PropTypes.func.isRequired
+    };
+
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                    <Navbar />
+                    <div className="container">
+                        <Alert />
+                        <Route exact path="/" component={Home} />
+                        <Switch>
+                            <Route exact path="/login" component={Login} />
+                            <Route exact path="/register" component={Register} />
+                            <Route exact path="/contact-us" component={ContactUs} />
+                            <Route exact path="/about" component={About} />
+                        </Switch>
+                    </div>
+                </Router>
+            </Provider>
+        );
+    }
+}
 
 export default App;
