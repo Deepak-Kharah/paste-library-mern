c976b156e0943ad8c2370fed2a68914da8bdc2c4
Deepak-Kharah <kharahdeepak009@gmail.com>
2020-04-18 22:32:34 +0530

    production settings

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -15,10 +16,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/d', require('./routes/api/dumps'));
 
-const PORT = process.env.PORT || 5000;
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
 
-app.get('/', (req, res) => {
-    res.send('API RUNNING');
-});
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
